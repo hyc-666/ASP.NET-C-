86b268a5f06a4bfe72a038b99043768890f02e1a
hyc_666 <2213671872@qq.com>
2019-12-20 12:47:15 +0800

    综合练习1-6

---
+++ 综合练习1-6/综合练习1-6/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace 综合练习1_6
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "我创建得首个ASP.NET 4.0网页！";
+        }
+    }
+}
